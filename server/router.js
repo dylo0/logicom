12677cd5bf228911a4e1ae361fabc2214dff10bf
Tomasz Dyl <tomekdyl@gmail.com>
2014-06-06 01:01:10 +0200

    basic router setup

--- server/router.js
+++ server/router.js
@@ -1,11 +1,30 @@
 'use strict';
 
-module.exports = {
-	setupRoutes: function (app) {
-		app.get('/', this.getIndex);
-	},
+module.exports = function (app, passport) {
+	
+	app.get('/', function (req, res) {
+		res.render(index.ejs)
+	});
 
-	getIndex: function (req, res) {
-		res.sendfile( path.join( __dirname, '../app/index.html' ) );
-	}
+	app.get('/application', isLoggedIn, function (req, res) {
+		res.sendfile( path.join( __dirname, '../app/app.html' ) );
+	});
+
+	app.post('/login',  passport.authenticate('local-login'), {
+		successRedirect: '/application',
+		failureRedirect: '/',
+		failureFlash: true
+	});
+
+	app.get('/logout', function (req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+}
+
+function isLoggedIn (req, res, next) {
+	if (req.isAuthenticated())
+		return next();
+
+	res.redirect('/');
 }
