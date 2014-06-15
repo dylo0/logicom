267022ca0bf97ef7fd41a900445018f5ba1fad16
Tomasz Dyl <tomekdyl@gmail.com>
2014-06-15 17:08:08 +0200

    [Admin-panel] Add Modal for app confirmations and infos

--- admin_panel/app/scripts/controllers/editUserCtrl.js
+++ admin_panel/app/scripts/controllers/editUserCtrl.js
@@ -9,21 +9,38 @@
  */
 
 angular.module('adminPanelApp')
-  .controller('EditUserCtrl', function ($scope, $modalInstance, user) {
+  .controller('EditUserCtrl', function ($scope, $modalInstance, $modal, user) {
     $scope.user = user;
-
     $scope.resetPassword = function (user) {
-      // to be implemented
-      // var uid = user.logicomId;
-      // $http.post('admin/password_reset/' + uid).
-      //   success(function (data, status) {
-      //     console.log('password recovery request sent');
-      //     // add flash message notice here
-      //   }).
-      //   error(function (data, status) {
-      //     console.log(data);
-      //   })
-      console.log('requested password reset for ' + user.name);
+      var confirm = $modal.open({
+        templateUrl: 'views/modal.html',
+        controller: 'ModalCtrl',
+        size: "sm",
+        resolve: {
+          options: function () {
+            var params = {
+              message: "Are you really sure you want to reset password?",
+              title: "Confirm Action",
+              cancelButton: true
+            };
+            return params;
+          }
+        }
+      });
+      
+      confirm.result.then(function () {
+        // to be implemented
+        // var uid = user.logicomId;
+        // $http.post('admin/password_reset/' + uid).
+        //   success(function (data, status) {
+        //     console.log('password recovery request sent');
+        //     // add flash message notice here
+        //   }).
+        //   error(function (data, status) {
+        //     console.log(data);
+        //   })
+        console.log("Password reset request sent to user email @ " + user.email);
+      });
     }
 
     $scope.ok = function () {
---
+++ admin_panel/app/scripts/controllers/modalCtrl.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adminPanelApp.controller:ModalCtrl
+ * @description
+ * # ModalCtrl
+ * Controller of the adminPanelApp
+ */
+angular.module('adminPanelApp')
+  .controller('ModalCtrl', function ($scope, $modalInstance, $timeout, options) {
+    $scope.msg = options.message;
+    $scope.title = options.title;
+    $scope.cancelButton = (options.cancelButton !== 'undefined') ? options.cancelButton : true;
+
+    $scope.ok = function () {
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+
+  });
