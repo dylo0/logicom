1f97f043e2ea14b09a7aa92e73d4f06d36b5bb66
Tomasz Dyl <tomekdyl@gmail.com>
2015-01-03 03:50:05 +0100

    Finished loadMarket styles

--- app/scripts/config/datatablesCfg.js
+++ app/scripts/config/datatablesCfg.js
@@ -2,23 +2,27 @@ define(['app'], function ( AppManager) {
 	var tableHeight = $(window).height() - 400;
 		
 	$(window).resize(function() {
-	  $('.dataTables_scrollBody').css('height', ($(window).height() - 400));
+	  $('.dataTables_scrollBody').css('height', ($(window).height() - 370));
 	});
 
 	dataTablesCfg = {
 		scrollY:        tableHeight,
         scrollCollapse: true,
         pageLength: 100,
+        lengthChange: false,
 		language: {
 		    "sProcessing":   "Przetwarzanie...",
 		    "sLengthMenu":   "Pokaż _MENU_ pozycji",
 		    "sZeroRecords":  "Nie znaleziono pasujących pozycji",
 		    "sInfoThousands":  " ",
-		    "sInfo":         "Pozycje od _START_ do _END_ z _TOTAL_ łącznie",
+		    "sInfo":         "Pozycje od _START_ do _END_",
+		    // "sInfo":         "Pozycje od _START_ do _END_ z _TOTAL_ łącznie",
 		    "sInfoEmpty":    "Pozycji 0 z 0 dostępnych",
 		    "sInfoFiltered": "(filtrowanie spośród _MAX_ dostępnych pozycji)",
 		    "sInfoPostFix":  "",
-		    "sSearch":       "Szukaj:",
+		    // "sSearch":       "Szukaj:",
+		    "search": 		 "",
+		    "searchPlaceholder": " Szukaj",
 		    "sUrl":          "",
 		    "oPaginate": {
 		        "sFirst":    "Pierwsza",
